1da2be29ec5ade379a5636831cc90bf7f67e7710
tahatms12 <tahatms@gmail.com>
2025-06-17 00:20:04 +0500

    Add Node functions and setup

---
+++ functions/make-call.js
@@ -0,0 +1,32 @@
+const twilio = require('twilio');
+
+exports.handler = async (event) => {
+  let data;
+  try {
+    data = JSON.parse(event.body || '{}');
+  } catch (err) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: 'Invalid JSON' })
+    };
+  }
+
+  const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+
+  try {
+    const call = await client.calls.create({
+      url: data.url || 'http://demo.twilio.com/docs/voice.xml',
+      to: data.to,
+      from: process.env.TWILIO_PHONE_NUMBER
+    });
+    return {
+      statusCode: 200,
+      body: JSON.stringify(call)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message })
+    };
+  }
+};
